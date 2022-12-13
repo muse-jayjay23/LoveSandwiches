90c17ea344a1a645cda5763b238a73d0163b41c2
muse-jayjay23 <joshuamjones23@hotmail.com>
2022-12-13 16:39:08 +0000

    Fucntion to calculate surplus data

--- run.py
+++ run.py
@@ -17,6 +17,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -49,36 +52,49 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    return True
 
+    return True
 
 
 def update_sales_worksheet(data):
     """
-    Update the sales worksheet with the list data provided
+    Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully\n")
+    print("Sales worksheet updated successfully.\n")
+
 
 def calculate_surplus_data(sales_row):
     """
-    Creates a fucntion to calculate surplous between stock and sales data
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
-    print("Calculating surplus data... \n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1] 
-    print(stock_row)
+    stock_row = stock[-1]
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
 
 def main():
     """
-    Create main function to group othe rcalls to functions in the programme
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
-print("Welcome to Love Sandwich data automation!\n")
+
+print("Welcome to Love Sandwiches Data Automation")
 main()
